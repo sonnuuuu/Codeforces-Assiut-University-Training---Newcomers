1edf9b7539b0b5e09292a2ec2b81cfabb203d313
Sonu Kumar <96771055+sonnuuuu@users.noreply.github.com>
2022-08-31 17:04:48 +0530

    Create F - Digits Summation.cpp

---
+++ Sheet #1 (Data type -Conditions)/F - Digits Summation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    
+    long long n,m,sum;
+    cin>>n>>m;
+    n=n%10;
+    m=m%10;
+    sum=n+m;
+    cout<<sum<<endl;
+	return 0;
+}
