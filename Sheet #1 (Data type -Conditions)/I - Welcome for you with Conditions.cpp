86fca635682423536ed5ae4e3a26c7d9dbb2bd1f
Sonu Kumar <96771055+sonnuuuu@users.noreply.github.com>
2022-08-31 17:07:10 +0530

    Create I - Welcome for you with Conditions.cpp

---
+++ Sheet #1 (Data type -Conditions)/I - Welcome for you with Conditions.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    long long a,b;
+    cin>>a>>b;
+    if(a>=b){
+        cout<<"Yes";
+    }
+        else{
+            cout<<"No";
+        }
+return 0;
+}
