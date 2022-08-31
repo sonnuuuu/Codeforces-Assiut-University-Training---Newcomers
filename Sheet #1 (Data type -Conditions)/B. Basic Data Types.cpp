f3207260bff45348ea0fae8a0c6453d5314f7807
Sonu Kumar <96771055+sonnuuuu@users.noreply.github.com>
2022-08-31 17:00:58 +0530

    Create B. Basic Data Types.cpp

---
+++ Sheet #1 (Data type -Conditions)/B. Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    int i;
+    long long l;
+    char c;
+    float f;
+    double d;
+    cin>>i>>l>>c>>f>>d;;
+    cout<<i<<endl<<l<<endl<<c<<endl<<f<<endl<<d<<endl;
+    
+    return 0;
+}
