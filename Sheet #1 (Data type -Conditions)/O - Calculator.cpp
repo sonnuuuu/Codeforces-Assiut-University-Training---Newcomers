d0e1bd2270f53895848062c32a0f5a60ad9b71ba
Sonu Kumar <96771055+sonnuuuu@users.noreply.github.com>
2022-09-01 18:43:15 +0530

    Create O - Calculator.cpp

---
+++ Sheet #1 (Data type -Conditions)/O - Calculator.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    char c;
+    cin>>a>>c>>b;
+    if (c=='+') cout<<a+b;
+    else if(c=='-') cout<<a-b;
+    else if(c=='*') cout<<a*b;
+    else cout<<a/b;
+    return 0;
+}
